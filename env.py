7c7d1ff37ce71d604aa690e553fa78e16c7da785
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-11 11:43:28 +0100

    Added ElephantSQL DB and env.py

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ[
+    "DATABASE_URL"
+] = "postgres://knjvooah:Add5b7yK_m7vZel0HkOrZwMtQQJzvXaX@trumpet.db.elephantsql.com/knjvooah"
+
+os.environ["SECRET_KEY"] = "supersecretMASTERkeyhaha"
