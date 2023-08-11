63a16110d15f1b2b16c0293eeb545df613220e6c
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-11 12:55:18 +0100

    Updated Allowed Hosts in settings.py

--- coolcoders/settings.py
+++ coolcoders/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "https://coolcoders-b69260c4617a.herokuapp.com/",
+    "coolcoders-b69260c4617a.herokuapp.com",
     "localhost",
     "127.0.0.1",
     "127.0.0.1:8000",
