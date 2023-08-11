32fc00bd30b548a3edb1eda459fdb3098a032f1e
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-11 12:00:22 +0100

    Removed cashed env.py and venv folders

--- coolcoders/settings.py
+++ coolcoders/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile("env.py"):
     import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -40,7 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
+    "cloudinary_storage",
     "django.contrib.staticfiles",
+    "cloudinary",
     "blog",
 ]
 
@@ -116,7 +119,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+MEDIA_URL = "/media/"
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
 STATIC_URL = "/static/"
+STATICFILES_STORAGE = "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
+
+STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- env.py
+++ env.py
@@ -5,3 +5,7 @@ os.environ[
 ] = "postgres://knjvooah:Add5b7yK_m7vZel0HkOrZwMtQQJzvXaX@trumpet.db.elephantsql.com/knjvooah"
 
 os.environ["SECRET_KEY"] = "supersecretMASTERkeyhaha"
+
+os.environ[
+    "CLOUDINARY_URL"
+] = "cloudinary://344428465783876:9g4JQVwfLHgjMXxcgaJxHWj-Zr0@barneslow"
