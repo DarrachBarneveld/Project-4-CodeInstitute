d117a5f6ec89044b644610e3f6da6a92fd2dd220
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-11 14:29:57 +0100

    Hooked up templates and navbar navigation for authentication

--- coolcoders/settings.py
+++ coolcoders/settings.py
@@ -77,7 +77,7 @@ ROOT_URLCONF = "coolcoders.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [TEMPLATES_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
