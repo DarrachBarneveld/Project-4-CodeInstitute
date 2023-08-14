b61b7c95952aa723949cdd1ef22361737e122d1e
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-15 00:05:25 +0100

    Rendered categories to index tempalte as buttons

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import generic, View
-from .models import Post
+from .models import Post, Category
 
 
 # Create your views here.
+class CategoryList(generic.ListView):
+    model = Category
+    queryset = Category.objects.all()
+    template_name = "index.html"
 
 
 class PostList(generic.ListView):
