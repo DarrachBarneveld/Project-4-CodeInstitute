9931e0ecbf5579836f90e930fb94da7c06ef036b
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-27 15:09:58 +0100

    Styled profile page, added jquery counter

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,24 @@ setTimeout(() => {
   let alert = new bootstrap.Alert(messages);
   alert.close();
 }, 2500);
+
+$(".counter").each(function () {
+  const $this = $(this),
+    countTo = $this.attr("data-to");
+  countDuration = parseInt($this.attr("data-duration"));
+  $({ counter: $this.text() }).animate(
+    {
+      counter: countTo,
+    },
+    {
+      duration: countDuration,
+      easing: "linear",
+      step: function () {
+        $this.text(Math.floor(this.counter));
+      },
+      complete: function () {
+        $this.text(this.counter);
+      },
+    }
+  );
+});
