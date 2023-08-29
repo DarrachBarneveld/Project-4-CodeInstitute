1c18cf27cb0e0c2b19177892b185b74dbf4a7309
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-29 19:19:10 +0100

    Added routing to author page from post detail

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.CategoryList.as_view(), name="home"),
     path("add_post/", views.AddPost.as_view(), name="add_post"),
     path("profile/", views.Profile.as_view(), name="profile"),
+    path("profile/<slug:slug>/", views.Profile.as_view(), name="profile"),
     path("update_profile/", views.UpdateProfileView.as_view(), name="update_profile"),
     path("<slug:slug>/", views.CategoryPosts.as_view(), name="category_posts"),
     path("post/<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
