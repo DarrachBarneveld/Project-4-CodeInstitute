30f30ed5918f5824cfe64996499b607eb92cc8e5
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-08-29 19:35:34 +0100

    Added slug for profile detail page

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ from django.urls import path
 urlpatterns = [
     path("", views.CategoryList.as_view(), name="home"),
     path("add_post/", views.AddPost.as_view(), name="add_post"),
-    path("profile/", views.Profile.as_view(), name="profile"),
     path("profile/<slug:slug>/", views.Profile.as_view(), name="profile"),
     path("update_profile/", views.UpdateProfileView.as_view(), name="update_profile"),
     path("<slug:slug>/", views.CategoryPosts.as_view(), name="category_posts"),
