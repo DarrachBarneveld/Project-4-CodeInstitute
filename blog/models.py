0d50db88b2a15c1f255e355ab23d617246ac4efd
Darrach Barneveld <darrachbarneveld91@gmail.com>
2023-09-22 01:14:41 +0100

    Added css validation on touch

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from cloudinary.models import CloudinaryField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-    bio = models.TextField()
+    bio = models.TextField(blank=True)
 
     def __str__(self):
         return str(self.user)
@@ -57,7 +57,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    name = models.CharField(max_length=80)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
